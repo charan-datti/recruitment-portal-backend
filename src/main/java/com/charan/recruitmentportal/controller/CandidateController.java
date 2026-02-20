55525b1866526853953f8dd04b3275e1dbdc0cf8
Charan Datti <charan@Charans-MacBook-Air.local>
2026-02-20 15:00:30 -0600

    Introduce DTOs for Candidate API

--- src/main/java/com/charan/recruitmentportal/controller/CandidateController.java
+++ src/main/java/com/charan/recruitmentportal/controller/CandidateController.java
@@ -5,6 +5,9 @@ import com.charan.recruitmentportal.repository.CandidateRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 
 
@@ -25,8 +28,10 @@ public class CandidateController {
 
 
     @GetMapping
-    public List<Candidate> getAllCandidates() {
-        return candidateRepository.findAll();
+    public Page<Candidate> getAllCandidates(
+            @PageableDefault(size = 5, sort = "id") Pageable pageable
+    ) {
+        return candidateRepository.findAll(pageable);
     }
 
     @DeleteMapping("/{id}")
